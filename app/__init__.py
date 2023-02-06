79971dd2da9b66e6fd5c642f22ac182dda122c04
Rachel Xiao <rachelxiao907@gmail.com>
2023-02-06 10:45:50 -0800

    back to normal

--- app/__init__.py
+++ app/__init__.py
@@ -11,30 +11,13 @@ with open("app/data.json") as file:
 
 @app.route('/')
 def index():
-<<<<<<< HEAD
     return render_template('index.html', title='Team Portfolio', url=os.getenv("URL"))
-=======
-    projects = [
-        {
-            "name": "Personal Library",
-            "link": "https://github.com/rachelxiao907/congenial-broccolis",
-            "description": "A web application that serves as an online library for users to store a selection of books and movies they search for."
-        },
-        {
-            "name": "Space Invaders",
-            "link": "https://github.com/rachelxiao907/APCSFinalProject",
-            "description": "This project is a version of the game Space Invaders done in Processing. It is an interactive game where the player controls the shooter by moving it left and right with the arrow keys and pressing the spacebar to shoot a bullet upwards that harms the aliens."
-        }
-    ]
-    return render_template('index.html', title="Rachel Xiao", projects=projects, url=os.getenv("URL"))
->>>>>>> 39ce3e5b6b7259218ca7212d955f008c89f50005
 
 def user_route(username):
     def user_func():
         return render_template(f'{username}.html', title=username, experiences=data[username]["workExperiences"], hobbies=data[username]["hobbies"], url=os.getenv("URL"))
     return user_func
 
-<<<<<<< HEAD
 with open("app/data.json") as file:
     data = json.load(file)
     for user in data:
@@ -48,8 +31,3 @@ def page_not_found(e):
 if __name__ == "__main__":
     app.debug = True
     app.run()
-=======
-@app.route('/traveling')
-def travel():
-    return render_template('travels.html', title="Traveling", url=os.getenv("URL"))
->>>>>>> 39ce3e5b6b7259218ca7212d955f008c89f50005
