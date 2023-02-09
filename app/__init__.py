9d43455ef1e34e5a52a287a972f3f0886c6ad67f
Mauricio Gomez <mo@Mauricios-MacBook-Air.local>
2023-02-09 17:49:48 -0500

    updated-index-with-picocss

--- app/__init__.py
+++ app/__init__.py
@@ -8,25 +8,27 @@ app = Flask(__name__, template_folder='templates')
 
 @app.route('/')
 def index():
-    with open("app/data.json") as file:
-        data = json.load(file)
-        return render_template('index.html', title='Team Portfolio', url=os.getenv("URL"), users=data["users"])
+    return render_template('index.html', title='Team Portfolio', url=os.getenv("URL"))
 
-def user_route(username):
-    def user_func():
-        return render_template(f'{username}.html', title=username, url=os.getenv("URL"))
-    return user_func
+@app.route('/helen')
+def helen():
+    return render_template('helen.html', title='Helen', url=os.getenv("URL"))
 
-with open("app/data.json") as file:
-    data = json.load(file)
-    for user in data["users"]:
-        first_name = user["firstName"].lower()
-        app.add_url_rule(f'/{first_name}', first_name, user_route(first_name))
+@app.route('/mauricio')
+def mauricio():
+    return render_template('mauricio.html', title='Mauricio', url=os.getenv("URL"))
+
+@app.route('/rachel')
+def rachel():
+    return render_template('rachel.html', title='Rachel', url=os.getenv("URL"))
+
+@app.route('/eliza')
+def eliza():
+    return render_template('eliza.html', title='Eliza', url=os.getenv("URL"))
 
 @app.errorhandler(404)
 def page_not_found(e):
     return render_template('404.html', title='404', url=os.getenv("URL")), 404
 
 if __name__ == "__main__":
-    app.debug = True
     app.run()
