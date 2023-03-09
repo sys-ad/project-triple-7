f477d796a9bade3592e40690fc91e7875b574f92
MoGo <95191519+sys-ad@users.noreply.github.com>
2023-03-09 18:21:07 -0500

    Create test_app.py

---
+++ tests/test_app.py
@@ -0,0 +1,42 @@
+import unittest
+import os
+from app import app
+
+os.environ['TESTING'] = 'true'
+
+class AppTestCase(unittest.TestCase):
+    def setUp(self):
+        self.client = app.test_client()
+
+    def test_home(self):
+        response = self.client.get("/")
+        assert response.status_code == 200
+        html = response.get_data(as_text=True)
+        assert "<title>MLH Fellowship '23'</title>" in html
+
+    def test_timeline(self):
+        response = self.client.get("/api/timeline_post")
+        assert response.status_code == 200
+        assert response.is_json
+        json = response.get_json()
+        assert "timeline_posts" in json
+        assert len(json["timeline_posts"]) != 0
+
+    def test_malformed_timeline_post(self):
+        # Test for invalid content
+        response = self.client.post("/api/timeline_post", data={"name": "John Doe", "email": "john@example.com", "content": ""})
+        assert response.status_code == 400
+        html = response.get_data(as_text=True)
+        assert "Invalid content" in html
+
+        # Test for invalid email
+        response = self.client.post("/api/timeline_post", data={"name": "John Doe", "email": "not-an-email", "content": "Hello world, I'm John!"})
+        assert response.status_code == 400
+        html = response.get_data(as_text=True)
+        assert "Invalid email" in html
+
+        # Test for missing name
+        response = self.client.post("/api/timeline_post", data={"email": "john@example.com", "content": "Hello world, I'm John!"})
+        assert response.status_code == 400
+        html = response.get_data(as_text=True)
+        assert "Name is required" in html
