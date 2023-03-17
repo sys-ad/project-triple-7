61d2445152aadaa7c4b1765ba8ac0aec8442ec83
MoGo <95191519+sys-ad@users.noreply.github.com>
2023-03-17 11:35:57 -0400

    Update redeploy-site.sh

--- redeploy-site.sh
+++ redeploy-site.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
+cd ~/portfolio_template_with_picocss/project-triple-7
 
-cd ~/project-triple-7/
-git fetch && git reset origin/main -hard
-source python3-virtualenv/bin/activate
-pip install -r requirements.txt
+git fetch && git reset origin/main --hard
 
-systemctl daemon-reload myportfolio
+docker compose down
+
+docker compose up -d --build
